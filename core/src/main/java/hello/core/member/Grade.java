eb52807af23f422f95f142f26be37a5d13f01b6a
jioniy <jw010208@gmail.com>
2022-12-18 19:51:32 +0900

    [ADD] #2 1-4

---
+++ core/src/main/java/hello/core/member/Grade.java
@@ -0,0 +1,9 @@
+package hello.core.member;
+
+/**
+ * 회원은 일반과 VIP 두 가지 등급이 있다.
+ */
+public enum Grade {
+    BASIC,
+    VIP
+}
---
+++ core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,23 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 회원 데이터는 자체 DB를 구축할 수 있고, 외부 시스템과 연동할 수 있다.(미확정)
+ */
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();//임시의 자체 DB 생성
+                                                            //HashMap 은 동시성 이슈가 발생할 수 있다. 이런 경우 ConcurrentHashMap 을 사용하자.
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
