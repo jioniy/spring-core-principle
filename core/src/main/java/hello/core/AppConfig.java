72914971cede196cbe14b96ad8ac7e8295390af2
jioniy <jw010208@gmail.com>
2023-01-08 15:26:53 +0900

    [ADD] #3 9 스프링으로 전환하기

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -9,8 +9,15 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
+ * 스프링으로 전환하기
+ * @Configuration - 설정을 구성함
+ * @Bean - 스프링 컨테이너에서 스프링 빈으로 자동 등록
+ * @Bean 이라 적힌 메서드를 모두 호출해서 반환된 객체를 스프링 컨테이너에 등록
+ *
  * 관심사의 분리
  * Application의 전체 동작 방식을 구성
  * 실제 동작에 필요한 구현 객체를 생성 및 연결 
@@ -21,19 +28,26 @@ import hello.core.order.OrderServiceImpl;
  * OrderServiceImpl
  * FixDiscountPolicy
  */
+
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(),discountPolicy());
     }
+
     /* 중복 제거*/
+    @Bean
     public MemberRepository memberRepository(){
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
         //return new FixDiscountPolicy();
         return new RateDiscountPolicy();
--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -1,15 +1,25 @@
 package hello.core;
 
 import hello.core.member.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
  * 직접적인 방식의 테스트는 좋은 방법이 아니다. 
  * JUnit을 사용할 것
+ *
+ * ApplicationContext : 스프링 컨테이너
  */
 public class MemberApp {
     public static void main(String[] args) {
+        /* 자바로 빈을 직접 등록 하는 경우
         AppConfig appConfig = new AppConfig();
         MemberService memberService = appConfig.memberService();
+         */
+        /* 스프링으로 빈을 자동 등록한 경우*/
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService",MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
